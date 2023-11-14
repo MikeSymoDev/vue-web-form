c60de6053a87cffc11250bd16536725801615e60
MikeSymoDev <mike04_norcal@yahoo.de>
2023-11-14 22:36:59 +0100

    skills

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,10 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required v-model="email">
+    <input type="email" required v-model="email" />
 
     <label>Password:</label>
-    <input type="password" required v-model="password">
+    <input type="password" required v-model="password" />
 
     <label>Role:</label>
     <select v-model="role">
@@ -12,8 +12,15 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+     <!-- <span @click="removeSkill(skill)">{{ skill }}</span>  -->
+     <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
     <div class="terms">
-      <input type="checkbox" v-model="terms" required>
+      <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
 
@@ -31,7 +38,6 @@
       <input type="checkbox" value="Horse" v-model="names">
       <label>Horse</label>
     </div> -->
-
   </form>
 
   <p>Email: {{ email }}</p>
@@ -49,49 +55,83 @@ export default {
       password: '',
       role: '',
       terms: false,
+      tempSkill: '',
+      skills: [],
       // names: []
-    }
-  }
+    };
+  },
+  methods: {
+    addSkill(e) { 
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
 
-}
+        this.tempSkill = '';
+      }
+    },
+  },
+  //   removeSkill(skill) {
+  //   console.log('hello');
+  //   this.skills = this.skills.filter((item) => {
+  //     return skill != item
+  //   })
+  // },
+  deleteSkill(skill) {
+      this.skills = this.skills.filter(item => {
+        return skill !== item
+      })
+    }
+}; 
 </script>
 
 <style>
 form {
-    max-width: 420px;
-    margin: 30px auto;
-    background: white;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
-  }
-  label {
-    color: #aaa;
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+ .pill {
     display: inline-block;
-    margin: 25px 0 15px;
-    font-size: 0.6em;
-    text-transform: uppercase;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
     letter-spacing: 1px;
     font-weight: bold;
-  }
-  input, select {
-    display: block;
-    padding: 10px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: 1px solid #ddd;
-    color: #555;
-  }
-  input[type="checkbox"] {
-    display: inline-block;
-    width: 16px;
-    margin: 0 10px 0 0;
-    position: relative;
-    top: 2px;
+    color: #777;
+    cursor: pointer;
   }
 </style>
-
-
 
 <!-- Lesson 41 -->
